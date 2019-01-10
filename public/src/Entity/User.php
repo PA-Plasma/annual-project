d07f1c9b28d1cd9d237ad5f7b4c2ed32fda12b74
waxime <aveline.maxime@gmail.com>
2019-01-11 00:21:47 +0100

    basic-controller: Define User entity as user_account for database

--- public/src/Entity/User.php
+++ public/src/Entity/User.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="user_account")
  */
 class User implements UserInterface
 {
