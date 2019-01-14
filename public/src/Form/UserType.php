29fb262106f3e974dfb15ee049262491dbc94a42
waxime <aveline.maxime@gmail.com>
2019-01-15 00:37:36 +0100

    auth: Add nickname to entity form type

--- public/src/Form/UserType.php
+++ public/src/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -46,6 +47,13 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add(
+            'nickname',
+            TextType::class,
+            [
+                'label' => 'Choose a cute nickname: '
+            ]
+        )
+        ->add(
             'email',
             EmailType::class,
             [
