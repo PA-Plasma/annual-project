d7ac82322513820018791d5a491b6e98b8f6eabb
tcrepy <tanguycrepy@gmail.com>
2019-01-15 02:16:55 +0100

    autocomplete-entrant: add jquery and bootstrap to project

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -19,7 +19,7 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('formEvent', './assets/js/formEvent.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // Bootstrap import
@@ -49,7 +49,7 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
