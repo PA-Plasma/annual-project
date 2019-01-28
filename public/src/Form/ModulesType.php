a0062bf58e972467a94fac3420adf29b9f2a70ca
waxime <aveline.maxime@gmail.com>
2019-01-28 23:06:54 +0100

    modules: Manage Modules form type

---
+++ public/src/Form/ModulesType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Modules;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ModulesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Name:',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-control mb-2',
+                    'placeholder' => 'Custom module'
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Modules::class,
+        ]);
+    }
+}
