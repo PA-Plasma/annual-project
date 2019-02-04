1dc3f8ffc10384b724ec7aed7c91ada25d47bf2d
waxime <aveline.maxime@gmail.com>
2019-02-04 09:58:42 +0100

    modules: WIP Manage modules root

---
+++ public/src/Service/ModulesHelper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+
+
+/**
+ * Class RolesHelper
+ *
+ * @category  Class
+ * @package   App\Service
+ */
+class ModulesHelper
+{
+    public function getModuleService($event)
+    {
+
+    }
+}
