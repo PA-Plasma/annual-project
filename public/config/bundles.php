ffa4083c2e1c9ea1a7604131b02d79f47d0007c5
waxime <aveline.maxime@gmail.com>
2019-06-13 00:08:53 +0200

    fixtures: Init fixtures and faker modules

--- public/config/bundles.php
+++ public/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
