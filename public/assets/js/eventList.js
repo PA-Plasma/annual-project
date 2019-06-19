a2d84d87f0e0cdb9bc1fadeb1a2b4fcd608704f8
tcrepy <tanguycrepy@gmail.com>
2019-06-19 23:54:10 +0200

    design: page acceuil et liste event

--- public/assets/js/eventList.js
+++ public/assets/js/eventList.js
@@ -1,6 +1,6 @@
 import Search from "./Search";
 
 $(document).ready(function () {
-    let le = new Search('#list-event', '/event/list/', '#search', '#display-search', '#rechercher', '.filtre-input')
+    let le = new Search('#list-event', '/event/list/', '#search', '#display-search', '#rechercher', '.filtre-input', '.skeleton');
     le.getContent();
 });
